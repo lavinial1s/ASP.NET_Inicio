7e139d8102b9f8347b54ef64ef6215225f5befce
lavinia <lislavinia19@gmail.com>
2025-05-27 20:45:58 -0300

    Inicio

---
+++ ASP.NET_Inicio/Models/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_Inicio.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+        public string? Nome {  get; set; }
+        public string? Email { get; set; }
+    }
+}
